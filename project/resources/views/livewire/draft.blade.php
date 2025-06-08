56a323c28a65e2860f81c9c11ff6479d800902a5
mutumba <rmutumba08@gmail.com>
2025-06-08 08:57:10 +0300

    Updated the landing page

--- project/resources/views/livewire/draft.blade.php
+++ project/resources/views/livewire/draft.blade.php
@@ -1,3 +1,22 @@
 <div>
     {{-- Because she competes with no one, no one can compete with her. --}}
+    <div class="relative mb-6 w-full">
+        <flux:heading size="xl" level="1">{{ __('Saved Draft') }}</flux:heading>
+        <flux:subheading size="lg" class="mb-6">{{ __('Manage your saved drafts and attachments.') }}</flux:subheading>
+        <flux:separator variant="subtle" />
+    </div>
+</br>
+    <div class="flex justify-end">
+        <flux:button type="submit" icon="paper-airplane" variant="primary" class="bg-blue-500 text-white hover:bg-blue-700 w-full">
+            {{ __('Send for Approving') }}
+        </flux:button>
+    </div>
+</br>
+    <div class="flex justify-end">
+        <flux:button type="submit" icon="paper-clip" variant="primary" class="bg-red-500 text-white hover:bg-red-700 w-full">
+            {{ __('Attachments') }}
+        </flux:button>
+    </div>
+
 </div>
+
--- project/resources/views/livewire/payment.blade.php
+++ project/resources/views/livewire/payment.blade.php
@@ -1,23 +1,22 @@
 <div>
-   
+
 
         <flux:navlist variant="outline">
                 <flux:navlist.group>
-                
+
 
                     <flux:navlist.item icon="plus" :href="route('newpayment')" :current="request()->routeIs('newpaymentpayment')" wire:navigate>
                       {{ __('New Payment') }}
                     </flux:navlist.item>
 
+                     <flux:navlist.item icon="folder-open" :href="route('draft')" :current="request()->routeIs('draft')" wire:navigate>
+                      {{ __('Draft') }}
+                    </flux:navlist.item>
+
                     <flux:navlist.item icon="check" :href="route('approve')" :current="request()->routeIs('approve')" wire:navigate>
                       {{ __('Approved') }}
                     </flux:navlist.item>
-
-                    <flux:navlist.item icon="folder-open" :href="route('draft')" :current="request()->routeIs('draft')" wire:navigate>
-                      {{ __('Draft') }}
-                    </flux:navlist.item>
-                    
-                     <flux:navlist.item icon="arrows-right-left" :href="route('pending')" :current="request()->routeIs('pending')" wire:navigate>
+                    <flux:navlist.item icon="arrows-right-left" :href="route('pending')" :current="request()->routeIs('pending')" wire:navigate>
                       {{ __('Pending') }}
                     </flux:navlist.item>
                     <flux:navlist.item icon="x-circle" :href="route('rejected')" :current="request()->routeIs('rejected')" wire:navigate>
@@ -25,8 +24,8 @@
                     </flux:navlist.item>
 
 
-                    
-                    
+
+
                 </flux:navlist.group>
 
             </flux:navlist>
